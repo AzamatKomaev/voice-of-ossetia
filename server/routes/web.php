7b6a3da1f02269f24b2a34dfceb9b9c671ccf232
Азамат Комаев <azamatkomaev15@gmail.com>
2022-07-17 22:29:47 +0300

    #67. Add Cors middleware. Add project pulling function in github-actions.yml.

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('media/', [\App\Http\Controllers\MediaController::class, 'get']);
+Route::get('media/', [\App\Http\Controllers\MediaController::class, 'get'])->middleware('cors');
