0a03966ab54b6fae1a60767e755bc32379a0f0db
Азамат Комаев <azamatkomaev15@gmail.com>
2022-07-19 13:58:10 +0300

    #77. Change CaptchaService.php.

--- server/app/Services/CaptchaService.php
+++ server/app/Services/CaptchaService.php
@@ -13,10 +13,10 @@ class CaptchaService
      */
     public static function verifyUser(string $captchaResponse): bool
     {
-        $response = Http::post(' https://www.google.com/recaptcha/api/siteverify', [
-            'secret' => env('CAPTCHA_SERVER_KEY'),
-            'response' => $captchaResponse
-        ]);
+        $response = Http::post('https://www.google.com/recaptcha/api/siteverify' .
+            '?secret=' . env('CAPTCHA_SERVER_KEY') .
+            '&response=' . $captchaResponse
+        );
         return ($response->ok() && $response->json('success'));
     }
 }
